7deec58e64cb526434aea684c10d03ac0ca63cf0
Ccjr0 <ccjr0@protonmail.com>
2021-06-16 14:25:30 -0500

    some patches

--- config.h
+++ config.h
@@ -6,17 +6,24 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "tamzen:size=12" };
 static const char dmenufont[]       = "tamzen:size=12";
 static const char col_gray1[]       = "#080808";
 static const char col_gray2[]       = "#121212";
 static const char col_gray3[]       = "#f1fcf8";
 static const char col_gray4[]       = "#f1fcf8";
+static const char new_color[]       = "#999999";
 static const char col_cyan[]        = "#292929";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  new_color },
+	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gray4, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_gray3, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray4, col_gray1,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_gray3, col_gray1,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
@@ -37,11 +44,14 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
+// #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "-tile-",      tile },    /* first entry is default */
 	{ "-flot-",      NULL },    /* no layout function means floating behavior */
 	{ "-mono-",      monocle },
+//	{ "[@]",         spiral },     
+//	{ "[\\]",         dwindle },   
 };
 
 /* key definitions */
@@ -68,16 +78,18 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_f,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_s,      focusstack,     {.i = -1 } },
-/*	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },        */ 
-/*	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },        */ 
+//	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+//	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-/*	{ MODKEY,                       XK_        zoom,           {0} },		*/
+//	{ MODKEY,                       XK_        zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-/*	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },*/
+//	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+//	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[3]} },
+//	{ MODKEY|ShiftMask,             XK_r,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_q,      setlayout,      {0} },
 	{ MODKEY,                       XK_y,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -92,7 +104,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_e,      shiftview,      { .i = +1 } },
 	{ MODKEY,                       XK_d,      shiftview,      { .i = -1 } },
 	TAGKEYS(                        XK_1,                      0)
-/*	TAGKEYS(                        XK_2,                      1)                   */
+//	TAGKEYS(                        XK_2,                      1)                   */
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
